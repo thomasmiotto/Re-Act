e69217dc4e891cc2612e4328ef6c5a5a17eafc57
Catoire-dev <cy.catoire.dev@gmail.com>
2023-05-29 11:02:16 +0200

    Debug compil (add import on empty module), add one card to app, create api folder with api file

--- src/Home/Card.tsx
+++ src/Home/Card.tsx
@@ -1,16 +1,17 @@
 import React from "react"
+import './Card.css'
 import { IFilm } from "../models/Film";
 
 type CardProps = {dataFilm : IFilm}
 
 export const Card = ({ dataFilm }: CardProps ) => {
   return (
-<figure>
-    <img src={dataFilm.img}/>
-    <figcaption>
-        <div>{dataFilm.title}</div>
-    </figcaption>
-</figure>
+    <figure>
+        <img src={dataFilm.img}/>
+        <figcaption>
+            <div>{dataFilm.title}</div>
+        </figcaption>
+    </figure>
 
   );
 };
