2a0003553ab0af073555dc54a4ae422a6ca0df61
thomasmiotto <thomasmiotto2@gmail.com>
2023-05-30 14:22:59 +0200

    new card

---
+++ src/pages/Home/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './Card.css'
+import { IFilm } from "../../models/Film";
+
+type CardProps = {dataFilm : IFilm}
+
+export const Card = ({ dataFilm }: CardProps) => {
+  return (
+    <div className="center">
+      <div className="article-card">
+        <div className="content">
+          <p className="title">{dataFilm.title}</p>
+        </div>
+        <img src={dataFilm.img} alt="article-cover" />
+      </div>
+    </div>
+  );
+};
+
