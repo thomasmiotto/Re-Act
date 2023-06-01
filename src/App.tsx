6280a4fcb9d53d04f9418faa30e094ad386b73fe
thomasmiotto <thomasmiotto2@gmail.com>
2023-06-01 15:30:29 +0200

    btn lock added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './pages/Home/Home';
+import { Header } from './componentsShared/Header';
 
 
 
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/DetailsCard" element={<DetailsCard />} /> */}
